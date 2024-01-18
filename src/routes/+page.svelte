60289aa6fe141690467cd59b8f93e9c45064c028
Auréliane Dor <aurelianedor@auremacbook.local>
2024-01-18 18:00:35 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { createScene, addRaclette } from './three';
+	import { onMount } from 'svelte';
+
+	let canvas;
+
+	onMount(() => {
+		createScene(canvas);
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => addRaclette()}>Ajouter une raclette</button>
+<canvas id="three" bind:this={canvas}></canvas>
